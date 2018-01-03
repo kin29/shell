04fe5a2ce7ed4106ed883404d8b1ac381dc082b1
shiga_ayano <shiga_ayano@aratana.jp>
2018-01-03 21:35:08 +0900

    標準入力とパイプについて

--- in_and_out.sh
+++ in_and_out.sh
@@ -3,7 +3,7 @@
 # 普通そのままに出力される
 echo hello!
 
-# 出力リダイレクト
+## 出力リダイレクト
 # 「hello!」という出力結果を新規ファイルoutput.txtに入力
 echo hello! > output.txt
 
@@ -15,4 +15,17 @@ date > output.txt
 # 既存ファイルoutput.txtに「add!」という出力結果を追記
 echo add! >> output.txt
 
+
+## 入力リダイレクト
+# cat output.txtと同じ結果!?
+cat < output.txt
+
+echo 6+1 > input.txt
+# input.txtの式6+1を計算し、結果を出力
+bc < input.txt
+
+
+## パイプ
+# 中間ファイルが不要となる
+echo 6+1 | bc
 
