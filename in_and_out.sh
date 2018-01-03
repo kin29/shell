d4b8b2cdafe2c1805d2742e9ebf8b62235726cc3
kin29 <kin29.com@gmail.com>
2018-01-03 21:20:23 +0900

    コメントアウト修正

--- in_and_out.sh
+++ in_and_out.sh
@@ -4,16 +4,15 @@
 echo hello!
 
 # 出力リダイレクト
-# hello!という出力が新規ファイルoutput.txtに入力された
+# 「hello!」という出力結果を新規ファイルoutput.txtに入力
 echo hello! > output.txt
 
 #「>」は上書き
-# 既存ファイルoutput.txtに上書き
+# 既存ファイルoutput.txtにdateコマンドの結果を上書き
 date > output.txt
 
-
 #「>>」は追記
-# 既存ファイルoutput.txtに追記
+# 既存ファイルoutput.txtに「add!」という出力結果を追記
 echo add! >> output.txt
 
 
