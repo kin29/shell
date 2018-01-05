370901b8045390afef6773361315b31d96781b0b
kin29 <kin29.com@gmail.com>
2018-01-05 21:23:52 +0900

    リダイレクト、パイプについても追記

--- in_and_out.sh
+++ in_and_out.sh
@@ -1,31 +1,40 @@
 #!/bin/sh
 
-# 普通そのままに出力される
+## 標準入出力
+# キーボードで入力(標準入力)したものがそのままに出力(標準出力)される
 echo hello!
 
+
 ## 出力リダイレクト
-# 「hello!」という出力結果を新規ファイルoutput.txtに入力
+# 「hello!」という出力結果を画面(標準出力)でなく新規ファイルoutput.txtに出力
 echo hello! > output.txt
 
 #「>」は上書き
-# 既存ファイルoutput.txtにdateコマンドの結果を上書き
+# dateコマンドの出力結果を既存ファイルoutput.txtに上書き
 date > output.txt
 
 #「>>」は追記
-# 既存ファイルoutput.txtに「add!」という出力結果を追記
+# 「add!」という出力結果を既存ファイルoutput.txtに追記
 echo add! >> output.txt
 
 
 ## 入力リダイレクト
-# cat output.txtと同じ結果!?
+# キーボード(標準入力)でなくファイルからの入力をコマンドに渡す
+# cat output.txtと同じ結果
+# cat 0< output.txtと同じ結果
 cat < output.txt
 
 echo 6+1 > input.txt
 # input.txtの式6+1を計算し、結果を出力
+# bc 0< input.txtと同じ結果
 bc < input.txt
+
+# bc < echo 6+1 はファイルからの入力ではないためできない。
 
 
 ## パイプ
-# 中間ファイルが不要となる
+# 標準出力をコマンドに渡したいとき
+# 中間ファイルが不要となる　
+# echo 6+1 > input.txt && bc < input.txt と同じ結果
 echo 6+1 | bc
 
